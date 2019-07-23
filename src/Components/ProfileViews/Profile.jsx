330ce02b158c25ba9175e0be0c146cf21322aca4
Student <example@example.com>
2019-07-22 22:20:05 -0700

    Add leaflet maps packages

--- src/Components/ProfileViews/Profile.jsx
+++ src/Components/ProfileViews/Profile.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import RideList from './RideViews/RideList';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
+import RideMap from './RideViews/MapViews/RideMap'
 
 export default function Profile(props) {
     return (
         <div>
-            {props.rideList.map((ride)=>{
+            {/* {props.rideList.map((ride)=>{
                 <div key={v4()}>
 
                 </div>
-            })}
+            })} */}
+            <RideMap/>
         </div>
     )
 }
---
+++ src/Components/ProfileViews/RideViews/MapViews/RideMap.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RideMap extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Map works lol</h1>
+            </div>
+        )
+    }
+}
