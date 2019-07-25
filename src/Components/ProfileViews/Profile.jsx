3cfda0b0bbabe718abde5ba7e3aa0a1420f796a3
Student <example@example.com>
2019-07-25 16:37:43 -0700

    clean syntax

--- src/Components/ProfileViews/Profile.jsx
+++ src/Components/ProfileViews/Profile.jsx
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import ProfileStats from './ProfileStatViews/ProfileStats';
 import RideList from './RideViews/RideList'
 import { connect } from 'react-redux';
-import { makeStyles } from "@material-ui/core"
 
-const styles = makeStyles(theme => ({
-	palette: {
-		primary: {
-			main: '#00897b'
-		},
-		secondary: {
-			main: '#009688'
-		},
-	},
-}));
+
 
 
 export default class Profile extends Component {
+   
     constructor(props){
         super(props);
         this.state = {
