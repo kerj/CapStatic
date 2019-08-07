abf4ff123cc1606d0b79532eb36cc3a36868c1fb
Justin Kerntz <kerntzj@gmail.com>
2019-08-06 23:01:40 -0400

    begin tests for persisting data in firebase

---
+++ __test__/profile-view-reducer.test.js
@@ -0,0 +1,4 @@
+import profileManipReducer from './../src/Reducers/profileManipReducer';
+import constants from './../src/Constants';
+const { c } = constants;
+
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,7 +1,8 @@
 import * as types from './ActionTypes';
 import { initialState } from './InitialState';
+import firebaseConfig from './firebaseConfig';
 
 export default {
     initialState: initialState,
-    types: types
+    c: types
 };
